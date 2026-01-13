763c7bed6daf72a6094c3de61c3607072aba7e66
Adam Lee Apalat <alsapalat@multisyscorp.com>
2026-01-13 15:40:36 +0800

    Add DebugLogs component to display query parameters in the Launcher

--- src/components/Launcher.tsx
+++ src/components/Launcher.tsx
@@ -5,7 +5,29 @@ import { AdSlider } from './AdSlider';
 import { CategoryFilter } from './CategoryFilter';
 import { AppGrid } from './AppGrid';
 
+const DebugLogs = () => {
+  const [show, setShow] = useState(false);
+  const queryParams = new URLSearchParams(window.location.search);
+  const queryParamsInJson = Object.fromEntries(queryParams.entries());
+  if (!show) {
+    return (
+      <div className="z-50 fixed bottom-0 left-0 w-full bg-black/40 text-white text-xs p-1 font-mono font-light overflow-auto">
+        <button className="text-underline w-full text-center" onClick={() => setShow(true)}>Show Debug Logs</button>
+      </div>
+    )
+  }
+  return (
+    <div className="z-50 fixed bottom-0 left-0 w-full h-40 bg-black/40 text-white text-xs p-1 font-mono font-light overflow-auto">
+      <div>Logs:</div>
+      <div>
+        <pre>{JSON.stringify(queryParamsInJson, null, 2)}</pre>
+      </div>
+    </div>
+  )
+}
+
 export function Launcher() {
+
   const [selectedCategory, setSelectedCategory] = useState<Category>('all');
 
   const filteredApps =
@@ -22,6 +44,7 @@ export function Launcher() {
         onSelect={setSelectedCategory}
       />
       <AppGrid apps={filteredApps} />
+      <DebugLogs />
     </div>
   );
 }
